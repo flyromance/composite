69a018aa48cf408320bb8a5e8522d2c1b6d09999
flyromance <814885004@qq.com>
2017-12-15 12:31:51 +0800

    fix

--- public/module/lazyload/loadscript.js
+++ public/module/lazyload/loadscript.js
@@ -62,33 +62,3 @@ var lazyload = (function () {
 })();
 
 
-var labjs = (function () {
-  var maps = {};
-
-  return {
-    script: function (url) {
-      loadScript(url, function () {
-        
-      })
-      return {
-        script: function () {
-
-        },
-        await: function () {
-
-        },
-      }
-    },
-    await: function () {
-
-      return {
-        script: function () {
-
-        },
-        await: function () {
-
-        }
-      }
-    }
-  }
-})()
